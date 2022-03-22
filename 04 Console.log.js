ef0d3e769ed9259c366e9c564642e68b78864cb0
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-22 22:46:11 +0900

    add console.log

---
+++ 04 Console.log.js
@@ -0,0 +1,16 @@
+//console.log(data);
+//소괄호 안에 작성된 값을 브라우저 콘솔 창에 출력
+//문자, 숫자 등을 출력
+
+//콤마로 구분하여 다수 작성 가능
+//소괄호 안에 작성한 값을 강좌에서는 파라미터 값이라고 한다.
+
+//콘솔로그는 자바스크립트가 아니라 console log API에서 제공한다.
+
+console.log(1+2);
+
+console.log(1, 2);
+
+var point = 123;
+console.log(point);
+log(point);
