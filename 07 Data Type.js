bd20b59dec8a4a6afc706f5183f0142ce50e10ec
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-23 00:08:57 +0900

    Add Data Type

---
+++ 07 Data Type.js
@@ -0,0 +1,41 @@
+//데이터 타입
+
+//데이터의 사전적 의미는 자료
+
+//데이터 타입 형태
+var value = 123;
+var value2 = "sports";
+
+//typeof 연산자
+//데이터(값) 타입 반환
+//typeof value에 데이터 작성
+
+
+console.log(typeof value);
+// = number
+
+console.log(typeof value2);
+// = string
+
+//키워드
+//특별한 기능을 가진 단어(ex: typeof)
+
+//데이터 타입
+
+//데이터 타입을 자료형이라고도 부름
+// * 강좌에서는 다이터 타입으로 표기
+
+// 데이터는 반드시 타입을 가짐****
+// JS는 데이터를 기준으로 타입을 결정
+// 타입을 먼저 선언하고 타입에 맞는 데이터를 선언하지 않음
+
+var point = 123;
+console.log(typeof point);
+// = number
+
+point = "첵";
+console.log(typeof point);
+// = string
+
+//즉 자바스크립트는 데이터를 읽어 봐야 데이터 타입을 알 수 있다.
+// 내가 사랑하는 자바는 데이터 타입을 먼저 선언함 ㅎ_ㅎ
