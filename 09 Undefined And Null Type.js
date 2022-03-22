611acd7c0ead13846ff9cf3d4c2da1492d732f96
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-03-23 00:56:15 +0900

    Create 09 Undefined And Null Type.js

---
+++ 09 Undefined And Null Type.js
@@ -0,0 +1,17 @@
+//Undefined 타입
+
+//Undefined(대문자) 타입
+// 값: undefined(소문자)
+
+//변수의 디폴트(Default)값
+//var point;
+//변수를 선언한 것 만으로 undefined가 초기값으로 설정
+
+var point;
+console.log(point);
+console.log("타입오브" + typeof point);
+//변수에 값을 할당하지 않은것을 나타내는 시멘틱
+
+//변수에 undefined 할당 가능
+point = undefined;
+console.log(point);
