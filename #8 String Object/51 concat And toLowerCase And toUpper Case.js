a5ecfc0f41682cdde3f5c8a1135157e1ae0f3a80
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-05 22:17:26 +0900

    add concat And toLowerCase And toUpper Case

---
+++ #8 String Object/51 concat And toLowerCase And toUpper Case.js
@@ -0,0 +1,24 @@
+/*
+    concat()
+    data = 연결 시작 값, String 인스턴스
+    파라미터 = 연결 대상 opt, 다수 작성 가능
+    반환 = 연결한 결과
+*/
+
+var result = "sports".concat("축구", 11);
+console.log(result);
+
+var obj = new String(123);
+console.log(obj.concat("ABC"))
+
+/*
+
+    toLowerCase()
+    영문 대문자를 소문자로 변환
+
+    toupperCase()
+    영문 소문자를 대무낮로 변환
+*/
+
+console.log("ABCD".toLowerCase);
+console.log("abcd".toUpperCase);
