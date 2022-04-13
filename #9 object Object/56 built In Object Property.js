cc7d546ff0b25791ad0ea31dc51f0974af03e700
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-14 00:51:07 +0900

    add built in object property

---
+++ #9 object Object/56 built In Object Property.js
@@ -0,0 +1,22 @@
+/*
+
+    built-in object property
+
+    프로퍼티 리스트
+
+    new Object() = 파라미터 데이터 타입의 인스턴스 생성
+    Object() = Object 인스턴스 생성
+    Object.prototype
+
+    constructor = 생성자
+
+    valueOf() = 프리미티브 값 반환
+    hasOwnProperty() = 프로퍼티 소유 여부 반환
+    propertyIsnumberalbe() 프로퍼티 열거 여부 반환
+    isPrototypeOf() = prototype의 존재 여부 반환
+    toString() = 문자열로 반환
+    toLocaleString() = 지역화 문자열로 변환
+
+    아래의 6개 값들은 인스턴스.proto의 .proto에 있다!
+
+*/
